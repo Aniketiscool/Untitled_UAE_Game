3e6e82feed47a1fe3f7c38d7fe574671becc0d0b
Aniketiscool <g.aniketiscool@gmail.com>
2026-06-03 11:21:08 +0400

    changed movement, added player state mananger
    new movement system and something to change sprites later based on actions

--- mars_game.py
+++ mars_game.py
@@ -5,10 +5,31 @@ import sys
 
 screen_width = 1280
 screen_height = 720
-
+player_state = "idle"
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
+Game_state = "title_screen"
+
+def run_game():
+    pygame.display.set_caption('Untitled_UAE_game')
+    while True:
+        dt = clock.tick(60) / 1000.0
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+        #Activates the player methods        
+        player.draw(screen)
+        player.movement(dt, pygame.key.get_pressed())
+        player.state_machine(pygame.key.get_pressed())
+
+
+        pygame.display.flip()
+        print(player.x, player.y, player_state)
 
 
 class Player:
@@ -20,48 +41,52 @@ class Player:
         self.current_health = 100
         self.damage = 20
         self.is_alive = True
+           
 
-    def main(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), self.radius)
+    def draw(self, screen):
+        pygame.draw.circle(screen, (126, 0, 126), (self.x, self.y), self.radius)
 
     def movement(self, dt, keypressed):
-
-        direction_x = 0
-        direction_y = 0
+        X_direction_to_move = 0
+        Y_direction_to_move = 0 
         if keypressed[pygame.K_w] or keypressed[pygame.K_UP]:
-            direction_y -= 1
+            Y_direction_to_move -= 1
+
         if keypressed[pygame.K_s] or keypressed[pygame.K_DOWN]:
-            direction_y += 1
+            Y_direction_to_move += 1
         if keypressed[pygame.K_a] or keypressed[pygame.K_LEFT]:
-            direction_x -= 1
+            X_direction_to_move -= 1
         if keypressed[pygame.K_d] or keypressed[pygame.K_RIGHT]:
-            direction_x += 1
+            X_direction_to_move += 1
 
-        if direction_x != 0 or direction_y != 0:
+        if X_direction_to_move != 0 or Y_direction_to_move != 0:
 
-            direction_x /= 0.7071
-            direction_y /= 0.7071
+            X_direction_to_move /= 0.7071
+            Y_direction_to_move /= 0.7071
 
-            self.x += direction_x * self.movementspeed * dt
-            self.y += direction_y * self.movementspeed * dt
+            self.x += X_direction_to_move * self.movementspeed * dt
+            self.y += Y_direction_to_move * self.movementspeed * dt
 
         self.x = max(self.radius, min(screen_width - self.radius, self.x))
         self.y = max(self.radius, min(screen_height - self.radius, self.y))
 
-player= Player(screen_width // 2, screen_height // 2, 20)
+    def state_machine(self,keypressed):
+        global player_state
+        if keypressed[pygame.K_w] or keypressed[pygame.K_UP]:
+            player_state = "moving_up"
+        if keypressed[pygame.K_s] or keypressed[pygame.K_DOWN]:
+            player_state = "moving_down"
+        if keypressed[pygame.K_a] or keypressed[pygame.K_LEFT]:
+            player_state = "moving_left"
+        if keypressed[pygame.K_d] or keypressed[pygame.K_RIGHT]:
+            player_state = "moving_right"
+        if not (keypressed[pygame.K_w] or keypressed[pygame.K_UP] or keypressed[pygame.K_s] or keypressed[pygame.K_DOWN] or keypressed[pygame.K_a] or keypressed[pygame.K_LEFT] or keypressed[pygame.K_d] or keypressed[pygame.K_RIGHT]):
+            player_state = "idle"
 
-def run_game():
-    pygame.display.set_caption('Untitled_UAE_game')
-    while True:
-        dt = clock.tick(60) / 1000.0
-        screen.fill((0, 0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        player.main(screen)
-        player.movement(dt, pygame.key.get_pressed())
-        pygame.display.flip()
+
+
+player= Player(screen_width // 2, 670, 20)
+
 
 
 if __name__ == '__main__':
